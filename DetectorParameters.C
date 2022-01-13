265e00dc445bfbd73197873194b151b1d3ce5ad7
Siddhant Rathi <siddhant@eic0101.rcf.bnl.gov>
2022-01-13 01:34:45 -0500

    progress update

---
+++ DetectorParameters.C
@@ -0,0 +1,21 @@
+#include<iostream>
+
+class Detector{
+
+private:  
+  Double_t circularCutXDthetaRadius = 0;
+  Double_t circularCutDphiRadius = 0;
+  Double_t etaCutEtaMin = 0;
+  Double_t etaCutEtaMax = 0;
+  TString sliceFittingFunction = 'gaus';
+  TString requiredEnergyResolution = "0.02 + 0.08/sqrt(x) + 0.02/x";
+  TString requiredEnergyResolutiuonLabel = "0.02 + 0.08/#sqrt{ge} + 0.02/ge";  
+  Double_t 
+  Double_t
+  Double_t
+  Double_t
+  Double_t
+
+
+
+} 
